2d7aa061946cc99ad08fb832a476b370578ed838
tdy <djsjtsh954@gmail.com>
2024-12-12 13:48:50 +0800

    为nvim配置格式化并增加yazi插件

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,11 +5,11 @@ require("bufferline").setup {
         -- 使用 nvim 内置lsp
         diagnostics = "nvim_lsp",
         -- 左侧让出 nvim-tree 的位置
-        offsets = {{
+        offsets = { {
             filetype = "NvimTree",
             text = "File Explorer",
             highlight = "Directory",
             text_align = "left"
-        }}
+        } }
     }
 }
--- lua/plugins/plugins-setup.lua
+++ lua/plugins/plugins-setup.lua
@@ -1,77 +1,112 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 
 local plugins = {
-  "nvim-tree/nvim-web-devicons", -- 文件树图标
-  "github/copilot.vim", --github copilot
-  "folke/tokyonight.nvim", -- 主题
-  "nvim-lualine/lualine.nvim",  -- 状态栏
-  "nvim-tree/nvim-tree.lua",  -- 文档树
-  "nvim-tree/nvim-web-devicons", -- 文档树图标
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl" },
-
-  "nvim-treesitter/nvim-treesitter", -- 语法高亮
-  "neovim/pynvim", --py的接口依赖
-  "simnalamburt/vim-mundo",--先前修改的列表
-  "pocco81/auto-save.nvim",
-  "nvimtools/none-ls.nvim", --格式化代码
-  {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim", -- 这个相当于mason.nvim和lspconfig的桥梁
-    "neovim/nvim-lspconfig"
-  },
-
-      -- 自动补全
-  "hrsh7th/nvim-cmp",
-  "hrsh7th/cmp-nvim-lsp",
-  "L3MON4D3/LuaSnip", -- snippets引擎，不装这个自动补全会出问题
-  "saadparwaiz1/cmp_luasnip",
-  "rafamadriz/friendly-snippets",
-  "hrsh7th/cmp-path", -- 文件路径
-
-  "numToStr/Comment.nvim", -- gcc和gc注释
-  "windwp/nvim-autopairs", -- 自动补全括号
-  "HiPhish/rainbow-delimiters.nvim", --彩色括号
-
-  "akinsho/bufferline.nvim", -- buffer分割线
-  "lewis6991/gitsigns.nvim", -- 左则git提示
-  "onsails/lspkind.nvim", --vscode式的自动补全框
-  "christoomey/vim-tmux-navigator", -- use "ctrl-hjkl" to switch between planes.
-  {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
+    ---@type LazySpec
     {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
+        "mikavilpas/yazi.nvim",
+        event = "VeryLazy",
+        keys = {
+            -- 👇 in this section, choose your own keymappings!
+            {
+                "<leader>-",
+                "<cmd>Yazi<cr>",
+                desc = "Open yazi at the current file",
+            },
+            {
+                -- Open in the current working directory
+                "<leader>cw",
+                "<cmd>Yazi cwd<cr>",
+                desc = "Open the file manager in nvim's working directory",
+            },
+            {
+                -- NOTE: this requires a version of yazi that includes
+                -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+                '<c-up>',
+                "<cmd>Yazi toggle<cr>",
+                desc = "Resume the last yazi session",
+            },
+        },
+        ---@type YaziConfig
+        opts = {
+            -- if you want to open yazi instead of netrw, see below for more info
+            open_for_directories = false,
+            keymaps = {
+                show_help = '<f1>',
+            },
+        },
     },
-   },
-  },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-  }
- 
+    "nvim-tree/nvim-web-devicons", -- 文件树图标
+    "github/copilot.vim",        --github copilot
+    "folke/tokyonight.nvim",     -- 主题
+    "nvim-lualine/lualine.nvim", -- 状态栏
+    "nvim-tree/nvim-tree.lua",   -- 文档树
+    "nvim-tree/nvim-web-devicons", -- 文档树图标
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl" },
+
+    "nvim-treesitter/nvim-treesitter", -- 语法高亮
+    "neovim/pynvim",                 --py的接口依赖
+    "simnalamburt/vim-mundo",        --先前修改的列表
+    "pocco81/auto-save.nvim",
+    "nvimtools/none-ls.nvim",        --格式化代码
+    {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim", -- 这个相当于mason.nvim和lspconfig的桥梁
+        "neovim/nvim-lspconfig"
+    },
+
+    -- 自动补全
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "L3MON4D3/LuaSnip", -- snippets引擎，不装这个自动补全会出问题
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets",
+    "hrsh7th/cmp-path",              -- 文件路径
+
+    "numToStr/Comment.nvim",         -- gcc和gc注释
+    "windwp/nvim-autopairs",         -- 自动补全括号
+    "HiPhish/rainbow-delimiters.nvim", --彩色括号
+
+    "akinsho/bufferline.nvim",       -- buffer分割线
+    "lewis6991/gitsigns.nvim",       -- 左则git提示
+    "onsails/lspkind.nvim",          --vscode式的自动补全框
+    "christoomey/vim-tmux-navigator", -- use "ctrl-hjkl" to switch between planes.
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    }
+
 }
 local opts = {} -- 注意要定义这个变量
 
